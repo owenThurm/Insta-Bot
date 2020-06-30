d33ac7373712b8f206edd2a9837e0da18a7ec564
owenThurm <owen.p.thurm@gmail.com>
2020-06-30 18:04:44 -0400

    Bot is more resilient to intra-commenting actions, Bot has more variability in sleep between actions, updated names of users commented on

--- Bot.py
+++ Bot.py
@@ -8,63 +8,76 @@ import math
 
 class InstagramBot:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, brand):
         self.username = username
         self.password = password
+        self.brand = brand
         self.previousComments = open("Instagram_Comment_Names", "a")
         self.previousCommentsUsernames = open("Instagram_Comment_Names", "r").read().split(" ")
         self.driver = webdriver.Chrome('/Users/othurm/Desktop/Insta-Bot/chromedriver')
         self.login()
+        self.search(self.brand)
+        self.getFollowerList()
 
     def login(self):
      self.driver.get('https://www.instagram.com/accounts/login/')
-     sleep(math.floor(random.random() * 2 + 2))
+     sleep(math.floor(random.random() * 3 + 2))
      self.driver.find_element_by_name("username").send_keys(self.username)
      self.driver.find_element_by_name("password").send_keys(self.password)
      self.driver.find_element_by_css_selector(
          "#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4) > button").click()
 
     def search(self, searchText):
-        sleep(math.floor(random.random() * 2 + 4))
+        sleep(math.floor(random.random() * 3 + 4))
         #click search bar
         self.driver.find_element_by_css_selector("#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.LWmhU._0aCwM > div").click()
         #type searchText
         self.driver.find_element_by_css_selector("#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.LWmhU._0aCwM > input").send_keys(searchText)
-        sleep(math.floor(random.random() * 2 + 3))
+        sleep(math.floor(random.random() * 3 + 3))
         #select first option
         self.driver.find_element_by_css_selector("#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.LWmhU._0aCwM > div:nth-child(4) > div.drKGC > div > a:nth-child(1)").click()
 
     def getFollowerList(self):
-        sleep(math.floor(random.random() * 2 + 2))
+        sleep(math.floor(random.random() * 3 + 2))
         #clicks the list of followers
         self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > ul > li:nth-child(2) > a").click()
 
     def getFollowerAfterScroll(self, followerNum):
-        sleep(math.floor(random.random() * 2 + 2))
+        sleep(math.floor(random.random() * 3 + 2))
         #gets the follower that corresponds to followerNum
         self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(followerNum) + ") > div > div.t2ksc > div.enpQJ > div.d7ByH > a").click()
 
 
     def getFollower(self, followerNum):
-        sleep(math.floor(random.random() * 2 + 2))
+        sleep(math.floor(random.random() * 3 + 2))
         #gets the follower that corresponds to followerNum
         self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(followerNum) + ") > div > div.Igw0E.IwRSH.YBx95.vwCYk > div > div > div > a").click()    
 
     def comment(self, commentText):
-       sleep(math.floor(random.random() * 2 + 2))
-       #click the latest post
-       self.driver.find_element_by_css_selector("#react-root > section > main > div > div._2z6nI > article > div > div > div:nth-child(1) > div:nth-child(1) > a").click()
-       sleep(math.floor(random.random() * 2 + 2))
-       self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span._15y0l > button").click()
-       sleep(math.floor(random.random() * 2 + 2))
-       self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea").send_keys(commentText)
-       sleep(math.floor(random.random() * 2 + 2))
-       self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > button").click()
-       sleep(math.floor(random.random() * 2 + 2))
-       self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.Igw0E.IwRSH.eGOV_._4EzTm.BI4qX.qJPeX.fm1AK.TxciK.yiMZG > button > svg").click()
-       sleep(math.floor(random.random() * 2 + 2))
-       self.driver.back()
-       self.driver.back()
+        try:    
+            sleep(math.floor(random.random() * 3 + 2))
+            #click the latest post
+            self.driver.find_element_by_css_selector("#react-root > section > main > div > div._2z6nI > article > div > div > div:nth-child(1) > div:nth-child(1) > a").click()
+            sleep(math.floor(random.random() * 3 + 2))
+            self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button").click()
+            sleep(math.floor(random.random() * 2 + 1))
+            self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span._15y0l > button").click()
+            sleep(math.floor(random.random() * 3 + 2))
+            self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea").send_keys(commentText)
+            sleep(math.floor(random.random() * 3 + 2))
+            self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > button").click()
+            sleep(math.floor(random.random() * 3 + 2))
+            self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.Igw0E.IwRSH.eGOV_._4EzTm.BI4qX.qJPeX.fm1AK.TxciK.yiMZG > button > svg").click()
+            sleep(math.floor(random.random() * 3 + 2))
+            self.driver.back()
+            self.driver.back()
+        except:
+            print('specific comment action failed')
+            sleep(math.floor(random.random() * 3 + 3))
+            self.driver.get('https://www.instagram.com/' + str(self.brand) + '/')
+            sleep(math.floor(random.random() * 3 + 3))
+            self.getFollowerList()
+
 
     def doCommentRound(self):
         j = 1
@@ -72,11 +85,15 @@ class InstagramBot:
         scrolled = False
         while i <= 15:
             try:
+                if(j > 5):
+                    scrolled = True
+                    target = self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(j) + ")")
+                    self.driver.execute_script('arguments[0].scrollIntoView(true);', target) 
                 if(scrolled):
                     self.getFollowerAfterScroll(j)
                 else:
                     self.getFollower(j)  
-                sleep(math.floor(random.random() * 2 + 4)) 
+                sleep(math.floor(random.random() * 3 + 3))    
                 try:   
                     username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h2").text
                 except:
@@ -84,6 +101,7 @@ class InstagramBot:
                 try:
                     if(self.followingUnder1500() and not self.alreadyCommented(username)):
                         self.previousComments.write(username + " ")
+                        print('commented on ' + username)
                         self.comment(randomCommentText())
                         print("comment number " + str(i) + " on account number " + str(j))
                         i+=1
@@ -92,13 +110,11 @@ class InstagramBot:
                         j+=1
                         self.driver.back()
                 except:
+                    print('failed to comment')
                     self.driver.back()
                     j+=1    
-                if(j > 5):
-                    scrolled = True
-                    target = self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(j) + ")")
-                    self.driver.execute_script('arguments[0].scrollIntoView(true);', target) 
             except:
+                print('overall comment round failed')
                 j+=1
         self.previousComments.close()         
 
@@ -111,12 +127,10 @@ class InstagramBot:
 
 
     def followingUnder1500(self):
-        sleep(math.floor(random.random() * 2 + 3))
+        sleep(math.floor(random.random() * 3 + 2))
         return (int(ig_bot.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a > span").text.replace(',', '')) < 1500)
     
 if __name__ == '__main__':
-    ig_bot = InstagramBot('owenthurm', 'Gandolf1')
-    ig_bot.search("riot society")
-    ig_bot.getFollowerList()
-    sleep(2)
+    ig_bot = InstagramBot('username', 'password', 'exact brand name')
+    sleep(math.floor(random.random() * 3 + 2))
     ig_bot.doCommentRound()
