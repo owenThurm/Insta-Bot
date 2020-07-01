6d65ad00bafb24dd2dc9915bee0ae449f45a09e7
owenThurm <owen.p.thurm@gmail.com>
2020-07-01 11:20:21 -0400

    Bot has sped up typing speed and another set of comments for the main account, which can be denoted with a boolean value in the constructor

--- Bot.py
+++ Bot.py
@@ -2,21 +2,26 @@ from selenium import webdriver
 import os
 import time
 from time import sleep
-from utility_methods import randomCommentText
+from utility_methods import randomCommentText 
+from utility_methods import randomCommentTextMain
 import random
 import math
 
 class InstagramBot:
 
-    def __init__(self, username, password, brand):
+    def __init__(self, username, password, brand, is_main):
         self.username = username
         self.password = password
         self.brand = brand
+        self.is_main = is_main
         self.previousComments = open("Instagram_Comment_Names", "a")
         self.previousCommentsUsernames = open("Instagram_Comment_Names", "r").read().split(" ")
         self.driver = webdriver.Chrome('/Users/othurm/Desktop/Insta-Bot/chromedriver')
+        sleep(random.random() * 3 + 3)
         self.login()
+        sleep(random.random() * 3 + 3)
         self.search(self.brand)
+        sleep(random.random() * 3 + 3)
         self.getFollowerList()
 
     def login(self):
@@ -24,11 +29,11 @@ class InstagramBot:
      sleep(math.floor(random.random() * 3 + 2))
      username_field = self.driver.find_element_by_name("username")
      for char in self.username:
-         sleep(random.random()/4)
+         sleep(random.random()/10)
          username_field.send_keys(char)
      password_field = self.driver.find_element_by_name("password")
      for char in self.password:
-         sleep(random.random()/4)
+         sleep(random.random()/10)
          password_field.send_keys(char)
      self.driver.find_element_by_css_selector(
          "#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4) > button").click()
@@ -40,7 +45,7 @@ class InstagramBot:
         #type searchText
         searchBox = self.driver.find_element_by_css_selector("#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.LWmhU._0aCwM > input")
         for char in searchText:
-            sleep(random.random()/4)
+            sleep(random.random()/10)
             searchBox.send_keys(char)
         sleep(math.floor(random.random() * 3 + 3))
         #select first option
@@ -75,7 +80,7 @@ class InstagramBot:
                 sleep(math.floor(random.random() * 3 + 2))
                 commentBox = self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea")
                 for char in commentText:
-                    sleep(random.random()/4)
+                    sleep(random.random()/10)
                     commentBox.send_keys(char)
                 sleep(math.floor(random.random() * 3 + 2))
                 self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > button").click()
@@ -123,7 +128,10 @@ class InstagramBot:
                     if(self.followingUnder1500() and not self.alreadyCommented(username)):
                         self.previousComments.write(username + " ")
                         print('commented on ' + username)
-                        self.comment(randomCommentText())
+                        if(self.is_main):
+                            self.comment(randomCommentTextMain())
+                        else:    
+                            self.comment(randomCommentText())
                         print("comment number " + str(i) + " on account number " + str(j))
                         i+=1
                         j+=1
@@ -154,6 +162,7 @@ class InstagramBot:
         return (int(ig_bot.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a > span").text.replace(',', '')) < 1500)
     
 if __name__ == '__main__':
-    ig_bot = InstagramBot('username', 'password', 'brand name')
+    #4th argument indicates whether or not the account is the main account
+    ig_bot = InstagramBot('username', 'password', 'brand name', False)
     sleep(math.floor(random.random() * 3 + 2))
     ig_bot.doCommentRound()
