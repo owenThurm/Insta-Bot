f278f980dc59d0e945d76d4d144e67c415a3b31e
Daniel Gelfand <33498710+DanielGelfand@users.noreply.github.com>
2020-09-03 18:11:50 -0400

    check if media already liked

--- Bot.py
+++ Bot.py
@@ -37,6 +37,7 @@ class InstagramBot:
      for char in self.password:
          sleep(random.random()/10)
          password_field.send_keys(char)
+     #self.driver.find_element_by_css_selector("#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4) > button").click()
      self.driver.find_elements_by_xpath("//*[text()='Log In']")[0].click()
 
     def search(self, searchText):
@@ -69,40 +70,46 @@ class InstagramBot:
         #gets the follower that corresponds to followerNum
         #self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(followerNum) + ") > div > div.Igw0E.IwRSH.YBx95.vwCYk > div > div > div > a").click()
         self.driver.find_elements_by_css_selector("a.FPmhX")[followerNum].click()
-    
+
     def comment(self, commentText):
         try:
             sleep(math.floor(random.random() * 3 + 2))
             #click the latest post
-
             print("Selecting latest post...")
             self.driver.find_element_by_css_selector("#react-root > section > main > div > div._2z6nI > article > div > div > div:nth-child(1) > div:nth-child(1) > a > div.eLAPa").click()
             #self.driver.find_element_by_css_selector("#react-root > section > main > div > div._2z6nI > article > div > div > div.NnQ7C.weEfm > div.v1Nh3.kIKUG ._bz0w > a > div.eLAPa").click()
             try:
                 print("Post selected...")
                 sleep(math.floor(random.random() * 3 + 2))
-                self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button").click()
-                print("Media liked.")
-                sleep(math.floor(random.random() * 2 + 1))
-                #print("here2")
-                self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span._15y0l > button").click()
-                sleep(math.floor(random.random() * 3 + 2))
-                commentBox = self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea")
-                for char in commentText:
-                    sleep(random.random()/10)
-                    commentBox.send_keys(char)
-                print("Media commented.")
-                sleep(math.floor(random.random() * 3 + 2))
-                # Hit the post button
-                self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > button").click()
-                sleep(math.floor(random.random() * 3 + 2))
-                #self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.Igw0E.IwRSH.eGOV_._4EzTm.BI4qX.qJPeX.fm1AK.TxciK.yiMZG > button > svg").click()
-                #print("Final click thing.")
-                #sleep(math.floor(random.random() * 3 + 2))
-                self.driver.back()
-                sleep(math.floor(random.random() * 3 + 1))
-                self.driver.back()
-                return True
+                if (not self.alreadyLiked()):
+                    self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button").click()
+                    print("Media liked.")
+                    sleep(math.floor(random.random() * 2 + 1))
+                    #print("here2")
+                    self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span._15y0l > button").click()
+                    sleep(math.floor(random.random() * 3 + 2))
+                    commentBox = self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea")
+                    for char in commentText:
+                        sleep(random.random()/10)
+                        commentBox.send_keys(char)
+                    print("Media commented.")
+                    sleep(math.floor(random.random() * 3 + 2))
+                    # Hit the post button
+                    self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > button").click()
+                    sleep(math.floor(random.random() * 3 + 2))
+                    #self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.Igw0E.IwRSH.eGOV_._4EzTm.BI4qX.qJPeX.fm1AK.TxciK.yiMZG > button > svg").click()
+                    #print("Final click thing.")
+                    #sleep(math.floor(random.random() * 3 + 2))
+                    print("Commented: " + commentText)
+                    self.driver.back()
+                    sleep(math.floor(random.random() * 3 + 1))
+                    self.driver.back()
+                    return True
+                else:
+                    self.driver.back()
+                    sleep(math.floor(random.random() * 3 + 1))
+                    self.driver.back()
+                    return False
             except:
                 print('Commenting on post failed')
                 sleep(math.floor(random.random() * 3 + 2))
@@ -119,62 +126,63 @@ class InstagramBot:
             self.driver.back()
             return False
 
+
     def doCommentRound(self):
         j = 1
         i = 0
         scrolled = False
         while i < 15:
-            #try:
-            if(j > 5):
-                scrolled = True
-                target = self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(j) + ")")
-                self.driver.execute_script('arguments[0].scrollIntoView(true);', target)
-            if(scrolled):
-                self.getFollowerAfterScroll(j)
-                print("\nGot follow after scroll")
-            else:
-                self.getFollower(j)
-                print("\nGot follower not scrolled yet")
-            sleep(math.floor(random.random() * 3 + 3))
             try:
-                username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h2").text
-                print("Got username: " + username)
-            except:
-                username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h1").text
-                print("Got username: " + username)
-            try:
-                if(self.followingUnder1500() and not self.alreadyCommented(username)):
-                    now = datetime.now()
-                    if(self.is_main):
-                        if(self.comment(randomCommentTextMain())):
-                            i+=1
-                            print('Commented on ' + username)
-                            print("(Comment number " + str(i) + " on account number " + str(j) + ") <----------------------- At " + str(now.hour % 12) + ":" + str(now.minute))
-                            self.previousComments.write(username + " ")
-                            print("Added username " + username)
-                    else:
-                        if(self.comment(randomCommentText())):
-                            i+=1
-                            print('Commented on ' + username)
-                            print("(Comment number " + str(i) + " on account number " + str(j) + ") <----------------------- At " + str(now.hour % 12) + ":" + str(now.minute))
-                            self.previousComments.write(username + " ")
-                            print("Added username " + username)
-                    j+=1
+                if(j > 5):
+                    scrolled = True
+                    target = self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(j) + ")")
+                    self.driver.execute_script('arguments[0].scrollIntoView(true);', target)
+                if(scrolled):
+                    self.getFollowerAfterScroll(j)
+                    print("\nGot follow after scroll")
                 else:
-                    j+=1
+                    self.getFollower(j)
+                    print("\nGot follower not scrolled yet")
+                sleep(math.floor(random.random() * 3 + 3))
+                try:
+                    username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h2").text
+                    print("Got username: " + username)
+                except:
+                    username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h1").text
+                    print("Got username: " + username)
+                try:
+                    if(self.followingUnder1500() and not self.alreadyCommented(username)):
+                        if(self.is_main):
+                            if(self.comment(randomCommentTextMain())):
+                                i+=1
+                                print('Commented on ' + username)
+                                print("Comment number " + str(i) + " on account number " + str(j))
+                                self.previousComments.write(username + " ")
+                                print("Added username " + username)
+                        else:
+                            if(self.comment(randomCommentText())):
+                                i+=1
+                                print('Commented on ' + username)
+                                print("Comment number " + str(i) + " on account number " + str(j))
+                                self.previousComments.write(username + " ")
+                                print("Added username " + username)
+                        #i+=1
+                        j+=1
+                    else:
+                        j+=1
+                        self.driver.back()
+                except:
+                    print('Unable to comment')
                     self.driver.back()
-            except Exception as e:
-                print(traceback.format_exc())
-                print(e)
-                print('Unable to comment')
-                self.driver.back()
+                    j+=1
+            except:
+                print('Overall comment round failed')
                 j+=1
-            #except:
-            #    print('Overall comment round failed')
-            #    j+=1
-            if(j > 300):
-                break
+            if(j > 150):
+                return True
         self.previousComments.close()
+        return True
+
 
     def alreadyCommented(self, username):
         for commented_on_user in self.previousCommentsUsernames:
@@ -183,15 +191,24 @@ class InstagramBot:
         return False
 
     def alreadyLiked(self):
-        unlike_xpath = "//section/span/button/div/span[*[local-name()='svg']/@aria-label='Unlike']"
+        #unlike_xpath = "//section/span/button/div/span[*[local-name()='svg']/@aria-label='Unlike']"
+        unlike_path = "html > body > div > div > div > article > div > section > span > button > div.QBdPU > span > svg._8-yf5"
         try:
-            liked_elem = self.driver.find_elements_by_xpath(unlike_xpath)
+            aria_label = self.driver.find_element_by_css_selector(unlike_path).get_attribute("aria-label")
+
+            print("aria-label: " + aria_label)
+
+            '''liked_elem = self.driver.find_elements_by_xpath(unlike_xpath)
             if len(liked_elem) == 1:
                 print("Image already liked... moving on")
                 return True
             print("Image has not been previously liked.")
+            return False'''
+            if (aria_label == "Unlike"):
+                return True
             return False
         except:
+            print("Exception")
             return False
         #return False
 
