e003fa78705b8dde52339daa317488787a9337ec
danielgelfand <>
2021-01-01 22:17:24 -0500

    check if liked

--- Bot.py
+++ Bot.py
@@ -2,10 +2,11 @@ from selenium import webdriver
 import os
 import time
 from time import sleep
-from utility_methods import randomCommentText 
+from utility_methods import randomCommentText
 from utility_methods import randomCommentTextMain
 import random
 import math
+import PySimpleGUI as sg
 
 class InstagramBot:
 
@@ -16,7 +17,7 @@ class InstagramBot:
         self.is_main = is_main
         self.previousComments = open("Instagram_Comment_Names", "a")
         self.previousCommentsUsernames = open("Instagram_Comment_Names", "r").read().split(" ")
-        self.driver = webdriver.Chrome('/Users/othurm/Desktop/Insta-Bot/chromedriver')
+        self.driver = webdriver.Chrome('/Users/thedg/Desktop/Insta-Bot/chromedriver')
         sleep(random.random() * 3 + 3)
         self.login()
         sleep(random.random() * 3 + 3)
@@ -49,7 +50,7 @@ class InstagramBot:
             searchBox.send_keys(char)
         sleep(math.floor(random.random() * 3 + 3))
         #select first option
-        self.driver.find_element_by_css_selector("#react-root > section > nav > div._8MQSO.Cx7Bp > div > div > div.LWmhU._0aCwM > div:nth-child(4) > div.drKGC > div > a:nth-child(1)").click()
+        self.driver.find_element_by_css_selector("span.Ap253").click()
 
     def getFollowerList(self):
         sleep(math.floor(random.random() * 3 + 2))
@@ -65,14 +66,14 @@ class InstagramBot:
     def getFollower(self, followerNum):
         sleep(math.floor(random.random() * 3 + 2))
         #gets the follower that corresponds to followerNum
-        self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(followerNum) + ") > div > div.Igw0E.IwRSH.YBx95.vwCYk > div > div > div > a").click()    
+        self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(followerNum) + ") > div > div.Igw0E.IwRSH.YBx95.vwCYk > div > div > div > a").click()
 
     def comment(self, commentText):
-        try:    
+        try:
             sleep(math.floor(random.random() * 3 + 2))
             #click the latest post
             self.driver.find_element_by_css_selector("#react-root > section > main > div > div._2z6nI > article > div > div > div:nth-child(1) > div:nth-child(1) > a").click()
-            try:    
+            try:
                 sleep(math.floor(random.random() * 3 + 2))
                 self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button").click()
                 sleep(math.floor(random.random() * 2 + 1))
@@ -90,14 +91,14 @@ class InstagramBot:
                 self.driver.back()
                 sleep(math.floor(random.random() * 3 + 1))
                 self.driver.back()
-            except: 
+            except:
                 print('commenting on post failed')
                 sleep(math.floor(random.random() * 3 + 2))
                 #backs out of the post
                 self.driver.back()
                 sleep(math.floor(random.random() * 3 + 1))
                 #backs out of the profile
-                self.driver.back()   
+                self.driver.back()
         except:
             print('sselecting latest post failed')
             sleep(math.floor(random.random() * 3 + 3))
@@ -114,23 +115,23 @@ class InstagramBot:
                 if(j > 5):
                     scrolled = True
                     target = self.driver.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div.isgrP > ul > div > li:nth-child(" + str(j) + ")")
-                    self.driver.execute_script('arguments[0].scrollIntoView(true);', target) 
+                    self.driver.execute_script('arguments[0].scrollIntoView(true);', target)
                 if(scrolled):
                     self.getFollowerAfterScroll(j)
                 else:
-                    self.getFollower(j)  
-                sleep(math.floor(random.random() * 3 + 3))    
-                try:   
+                    self.getFollower(j)
+                sleep(math.floor(random.random() * 3 + 3))
+                try:
                     username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h2").text
                 except:
-                    username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h1").text    
+                    username = self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > div.nZSzR > h1").text
                 try:
                     if(self.followingUnder1500() and not self.alreadyCommented(username)):
                         self.previousComments.write(username + " ")
                         print('commented on ' + username)
                         if(self.is_main):
                             self.comment(randomCommentTextMain())
-                        else:    
+                        else:
                             self.comment(randomCommentText())
                         print("comment number " + str(i) + " on account number " + str(j))
                         i+=1
@@ -141,28 +142,45 @@ class InstagramBot:
                 except:
                     print('failed to comment')
                     self.driver.back()
-                    j+=1    
+                    j+=1
             except:
                 print('overall comment round failed')
                 j+=1
             if(j > 300):
-                break    
-        self.previousComments.close()         
+                break
+        self.previousComments.close()
 
 
     def alreadyCommented(self, username):
         for commented_on_user in self.previousCommentsUsernames:
             if(commented_on_user == username):
                 return True
-        return False        
+        return False
 
 
     def followingUnder1500(self):
         sleep(math.floor(random.random() * 3 + 2))
         return (int(ig_bot.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a > span").text.replace(',', '')) < 1500)
-    
+
 if __name__ == '__main__':
     #4th argument indicates whether or not the account is the main account
-    ig_bot = InstagramBot('username', 'password', 'brand name', False)
+    user1 = 'streetwearlatest'
+    user1_pass = 'Julia26'
+    main1 = 'genuineaesthetic'
+    main1_pass = 'MagicJohnson32!'
+
+    form = sg.FlexForm('Enter Your Information') # begin with blank form
+    layout = [
+          [sg.Text('Please enter your Instagram Name, Password, and Brand To Steal Followers From')],
+          [sg.Text('Instagram Name', size=(15, 1)), sg.InputText('')],
+          [sg.Text('Instagram Password', size=(15, 1)), sg.InputText('')],
+          [sg.Text('Target Brand', size=(15, 1)), sg.InputText('')],
+          [sg.Text('Is this your main account?', size=(20, 1)), sg.Checkbox('Yes'), sg.Checkbox('No', default=True)],
+          [sg.Submit(), sg.Cancel()]
+         ]
+    button, values = form.Layout(layout).Read()
+    #print(button, values[0], values[1], values[2],values[3])
+    form.close()
+    ig_bot = InstagramBot(values[0],values[1],values[2],values[3] == 'True')
     sleep(math.floor(random.random() * 3 + 2))
     ig_bot.doCommentRound()
--- utility_methods.py
+++ utility_methods.py
@@ -2,7 +2,8 @@ import random
 import math
 
 def randomCommentText():
-    comments = ["It’s a vibe! shoot @genuineaesthetic a dm when you can",
+    comments = ["Ay DM @genuineaesthetic real quick",
+                "It’s a vibe! shoot @genuineaesthetic a dm when you can",
                 "It’s a vibe! dm @genuineaesthetic when you can ",
                 "It’s a vibe! when you get the chance shoot @genuineaesthetic a dm",
                 "It’s a vibe! when you get the chance shoot a dm to @genuineaesthetic ",
@@ -28,34 +29,22 @@ def randomCommentText():
                 "It's a vibe fr! dm @genuineaesthetic when you get the time",
                 "It's a vibe fr! dm @genuineaesthetic when you get the chance",
                 "It's a vibe fr! dm @genuineaesthetic when you get a chance",
-                "It's a vibe fr! dm @genuineaesthetic whenever you can"
-                "Dope shot! dm @genuineaesthetic when you can",
-                "Dope shot! dm @genuineaesthetic when you can",
-                "Dope shot! dm @genuineaesthetic whenever you get the chance!",
-                "Dope shot! dm @genuineaesthetic when you get the chance",
-                "Dope shot! when you get the chance shoot a dm to @genuineaesthetic",
-                "Dope shot! when you’re free shoot a dm to @genuineaesthetic",
-                "Dope shot! when you’re free dm @genuineaesthetic!",
-                "Dope shot! when you get some time shoot a dm to @genuineaesthetic",
-                "Dope shot! shoot a dm to @genuineaesthetic when you get the time",
-                "Dope shot! shoot a dm to @genuineaesthetic when you get a chance",
-                "Dope shot! shoot a dm to @genuineaesthetic when you get the chance"
-                "Dope shot fr! shoot a dm to @genuineaesthetic when you get the time",
-                "Dope shot fr! shoot a dm to @genuineaesthetic whenever your get a chance",
-                "Dope shot fr! whenever you're free shoot a dm to @genuineaesthetic",
-                "Dope shot! dm @genuineaesthetic whenever you’re free!",
-                "Vibes! shoot @genuineaesthetic a dm whenever you get the chance",
-                "Vibes! when you get a chance shoot a dm to @genuineaesthetic",
-                "Vibes! shoot a dm to @genuineaesthetic when you can",
-                "Vibes! dm @genuineaesthetic whenever you get the time",
-                "Vibes fr! dm @genuineaesthetic whenever you get the chance",
-                "Vibes fr! dm @genuineaesthetic whenever you're free!",
-                "Vibes fr! dm @genuineaesthetic when you find the time!",
-                "Vibes! if you get a chance shoot a dm to @genuineaesthetic"
-                "it's a vibe fr! dm @genuineaesthetic when you can",
-                "vibes fr! dm @genuineaesthetic when you can",
-                "Vibes! whenever you get the time shoot a dm to @genuineaesthetic",
-                "Vibes! if you're free shoot a dm to @genuineaesthetic",
+                "It's a vibe fr! dm @genuineaesthetic whenever you can",
+                "Dope! dm @genuineaesthetic when you can",
+                "Dope! dm @genuineaesthetic when you can",
+                "Dope! dm @genuineaesthetic whenever you get the chance!",
+                "Dope! dm @genuineaesthetic when you get the chance",
+                "Dope! when you get the chance shoot a dm to @genuineaesthetic",
+                "Dope! when you’re free shoot a dm to @genuineaesthetic",
+                "Dope! when you’re free dm @genuineaesthetic!",
+                "Dope! when you get some time shoot a dm to @genuineaesthetic",
+                "Dope! shoot a dm to @genuineaesthetic when you get the time",
+                "Dope! shoot a dm to @genuineaesthetic when you get a chance",
+                "Dope! shoot a dm to @genuineaesthetic when you get the chance",
+                "Dope fr! shoot a dm to @genuineaesthetic when you get the time",
+                "Dope fr! shoot a dm to @genuineaesthetic whenever your get a chance",
+                "Dope fr! whenever you're free shoot a dm to @genuineaesthetic",
+                "Dope! dm @genuineaesthetic whenever you’re free!",
                 "Vibes! shoot a dm to @genuineaesthetic if you're free",
                 "Hope you're having a good day! when you get the chance shoot a dm to @genuineaesthetic",
                 "Hope you're having a good day! when you're free shoot a dm to @genuineaesthetic",
@@ -70,35 +59,9 @@ def randomCommentText():
                 "Hope you're having a good day! shoot a dm to @genuineaesthetic when you get a chance",
                 "Hope you're having a good day! shoot a dm to @genuineaesthetic whenever you get a chance",
                 "Hope you're having a good day! shoot a dm to @genuineaesthetic when you get the time",
-                "Hope you're having a good day! shoot a dm to @genuineaesthetic whenever you're free!"
-                "Dope style! shoot a dm to @genuineaesthetic when you can!",
-                "Dope style! dm @genuineaesthetic when you get the chance!",
-                "Dope style! dm @genuineaesthetic whenever you get the chance!",
-                "Dope style! dm @genuineaesthetic when you get a chance",
-                "Dope style! dm @genuineaesthetic when you get a chance!",
-                "Dope style! dm @genuineaesthetic whenever you can",
-                "Dope style! dm @genuineaesthetic whenever you get the time",
-                "Dope style! whenever you get the time shoot a dm to @genuineaesthetic",
-                "Dope style! whenever you can, shoot a dm to @genuineaesthetic",
-                "Dope style! whenever you get a chance shoot a dm to @genuineaesthetic",
-                "Dope style! whenever you get the chance, shoot a dm to @genuineaesthetic"
-                "Dope style fr! dm @genuineaesthetic when you're free",
-                "Dope style fr! if you get the chance, shoot a dm to @genuineaesthetic",
-                "Dope style fr! whenever you get the time shoot a dm to @genuineaesthetic",
-                "Dope style fr! whenever you're free shoot a dm to @genuineaesthetic",
-                "Dope style fr! Whenever you're free dm @genuineaesthetic",
-                "Dope style fr! Whenever you get the time dm @genuineaesthetic",
-                "Dope style fr! Whenever you get a chance shoot a dm to @genuineaesthetic",
-                "Dope style fr! Whenever you get the chance shoot a dm to @genuineaesthetic",
-                "Dope style fr! Whenever you get the chance dm @genuineaesthetic",
-                "Dope style fr! Whenever you get a chance dm @genuineaesthetic",
-                "Dope style fr! shoot a dm to @genuineaesthetic whenever you get the chance!",
-                "Dope style fr! shoot a dm to @genuineaesthetic whenever you can!",
-                "Dope style fr! shoot a dm to @genuineaesthetic when you get the time!",
-                "Dope style fr! shoot a dm to @genuineaesthetic whenever you get the time!",
-                "Dope style fr! shoot a dm to @genuineaesthetic whenever you get a chance!",
+                "Hope you're having a good day! shoot a dm to @genuineaesthetic whenever you're free!",
                 "That's whats up, if you get the chance shoot a dm to @genuineaesthetic",
-                "That's whats up, when you get the chacne shoot a dm to @genuineaesthetic",
+                "That's whats up, when you get the chance shoot a dm to @genuineaesthetic",
                 "That's whats up, shoot a dm to @genuineaesthetic whenever you can",
                 "That's whats up, when you get a chance dm @genuineaesthetic",
                 "That's whats up, whenever you find the time shoot a dm to @genuineaesthetic",
@@ -116,6 +79,8 @@ def randomCommentText():
                 "That's whats up, shoot a dm to @genuineaesthetic whenever you get the time!",
                 "That's whats up, shoot a dm to @genuineaesthetic whenever you get a chance",
                 "That's whats up, shoot a dm to @genuineaesthetic when you get the time!"]
+
+    #return u"Dope! shoot a dm to @genuineaesthetic when you get the chance \u2764"
     return comments[math.floor(random.random()*len(comments))]
 
 def randomCommentTextMain():
@@ -145,7 +110,7 @@ def randomCommentTextMain():
                 "It's a vibe fr! dm us when you get the time",
                 "It's a vibe fr! dm us when you get the chance",
                 "It's a vibe fr! dm us when you get a chance",
-                "It's a vibe fr! dm us whenever you can"
+                "It's a vibe fr! dm us whenever you can",
                 "Dope shot! dm us when you can",
                 "Dope shot! dm us when you can",
                 "Dope shot! dm us whenever you get the chance!",
@@ -156,7 +121,7 @@ def randomCommentTextMain():
                 "Dope shot! when you get some time shoot a dm to us",
                 "Dope shot! shoot a dm to us when you get the time",
                 "Dope shot! shoot a dm to us when you get a chance",
-                "Dope shot! shoot a dm to us when you get the chance"
+                "Dope shot! shoot a dm to us when you get the chance",
                 "Dope shot fr! shoot a dm to us when you get the time",
                 "Dope shot fr! shoot a dm to us whenever your get a chance",
                 "Dope shot fr! whenever you're free shoot a dm to us",
@@ -168,7 +133,7 @@ def randomCommentTextMain():
                 "Vibes fr! dm us whenever you get the chance",
                 "Vibes fr! dm us whenever you're free!",
                 "Vibes fr! dm us when you find the time!",
-                "Vibes! if you get a chance shoot a dm to us"
+                "Vibes! if you get a chance shoot a dm to us",
                 "it's a vibe fr! dm us when you can",
                 "vibes fr! dm us when you can",
                 "Vibes! whenever you get the time shoot a dm to us",
@@ -187,7 +152,7 @@ def randomCommentTextMain():
                 "Hope you're having a good day! shoot a dm to us when you get a chance",
                 "Hope you're having a good day! shoot a dm to us whenever you get a chance",
                 "Hope you're having a good day! shoot a dm to us when you get the time",
-                "Hope you're having a good day! shoot a dm to us whenever you're free!"
+                "Hope you're having a good day! shoot a dm to us whenever you're free!",
                 "Dope style! shoot a dm to us when you can!",
                 "Dope style! dm us when you get the chance!",
                 "Dope style! dm us whenever you get the chance!",
@@ -198,7 +163,7 @@ def randomCommentTextMain():
                 "Dope style! whenever you get the time shoot a dm to us",
                 "Dope style! whenever you can, shoot a dm to us",
                 "Dope style! whenever you get a chance shoot a dm to us",
-                "Dope style! whenever you get the chance, shoot a dm to us"
+                "Dope style! whenever you get the chance, shoot a dm to us",
                 "Dope style fr! dm us when you're free",
                 "Dope style fr! if you get the chance, shoot a dm to us",
                 "Dope style fr! whenever you get the time shoot a dm to us",
@@ -215,7 +180,7 @@ def randomCommentTextMain():
                 "Dope style fr! shoot a dm to us whenever you get the time!",
                 "Dope style fr! shoot a dm to us whenever you get a chance!",
                 "That's whats up, if you get the chance shoot a dm to us",
-                "That's whats up, when you get the chacne shoot a dm to us",
+                "That's whats up, when you get the chance shoot a dm to us",
                 "That's whats up, shoot a dm to us whenever you can",
                 "That's whats up, when you get a chance dm us",
                 "That's whats up, whenever you find the time shoot a dm to us",
@@ -232,7 +197,8 @@ def randomCommentTextMain():
                 "That's whats up, shoot a dm to us whenever you find the time!",
                 "That's whats up, shoot a dm to us whenever you get the time!",
                 "That's whats up, shoot a dm to us whenever you get a chance",
-                "That's whats up, shoot a dm to us when you get the time!"]
+                "That's whats up, shoot a dm to us when you get the time!",
+                "Dope, dm us when you free"]
     return comments[math.floor(random.random()*len(comments))]
 
 if __name__ == '__main__':
@@ -256,4 +222,4 @@ if __name__ == '__main__':
                 "Dope shot🤟 when you get the chance shoot a dm to @genuineaesthetic 🙏",
                 "Dope shot🙌 when you’re free shoot a dm to @genuineaesthetic👌",
                 "Dope shot🙏 when you’re free dm @genuineaesthetic🙌",
-                "Dope shot🙌 dm @genuineaesthetic whenever you’re free🙏"]   
+                "Dope shot🙌 dm @genuineaesthetic whenever you’re free🙏"]
