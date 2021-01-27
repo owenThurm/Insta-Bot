f85e3eedccc50824c3ccdf630932bddf38632f4d
owenThurm <owen.p.thurm@gmail.com>
2021-01-26 23:36:53 -0500

    Added sleep, fixed emoji button issue for posting comments

--- new_bot.py
+++ new_bot.py
@@ -8,7 +8,7 @@ import random
 import math
 from datetime import datetime
 from selenium.webdriver.common.action_chains import ActionChains
-import pyautogui
+#import pyautogui
 from threading import Thread
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
@@ -17,7 +17,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 
 PROXY = "91.229.245.187:21289" #HOST:PORT
 PROXY_USERNAME = "bobdill32984"
@@ -33,9 +32,9 @@ class InstagramBot:
         self.previousComments = open("Instagram_Comment_Names", "a")
         self.previousCommentsUsernames = open("Instagram_Comment_Names", "r").read().split(" ")
 
+        self.driver = webdriver.Chrome('/Users/othurm/Desktop/Insta-Bot/chromedriver')
 
-
-        chrome_options = webdriver.ChromeOptions()
+        #chrome_options = webdriver.ChromeOptions()
         #chrome_options.headless = True
         #chrome_options.add_argument("--headless") # Runs Chrome in headless mode.
         #chrome_options.add_argument('--no-sandbox') # Bypass OS security model
@@ -45,11 +44,10 @@ class InstagramBot:
         #chrome_options.add_argument("--disable-extensions")
         #chrome_options.add_argument('--proxy-server=%s' % PROXY)
 
-
-        chrome_options.add_argument("--window-size=1920x1080")
-        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-        chrome_options.add_argument(f'user-agent={user_agent}')
-        self.driver = webdriver.Chrome(options=chrome_options)#,seleniumwire_options=options)
+        #chrome_options.add_argument("--window-size=1920x1080")
+        #user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        #chrome_options.add_argument(f'user-agent={user_agent}')
+        #self.driver = webdriver.Chrome(options=chrome_options)#,seleniumwire_options=options)
         sleep(random.random() * 2)
 
         # Login to Your Account
@@ -58,8 +56,6 @@ class InstagramBot:
         self.get_brand()
         # Click on list of followers
         self.getFollowerList()
-
-
 
     def login(self):
         try:
@@ -93,8 +89,9 @@ class InstagramBot:
                 print("Failed to land in homepage>>>")
                 print(e)
 
-        except:
-            print("Login failed....")
+        except Exception as e:
+            print("Login failed....", e)
+        screenshot here
             #self.driver.save_screenshot("screenshot.png")
             #self.driver.close()
 
@@ -122,8 +119,7 @@ class InstagramBot:
             )
             sleep(random.random() / 2 + 0.1)
         except Exception as e:
-            print("Failed to wait for followers>>>")
-            print(e)
+            print("Failed to wait for followers>>>", e)
             #self.driver.quit()
 
 
@@ -155,7 +151,7 @@ class InstagramBot:
 
 
     def getFollower(self, followerNum):
-    
+
         try:
             element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.CSS_SELECTOR, "a.FPmhX"))
@@ -170,7 +166,6 @@ class InstagramBot:
 
     def comment(self, commentText):
         try:
-           
             # Click the latest post
             print("Selecting latest post...")
             self.driver.find_element_by_css_selector("#react-root > section > main > div > div._2z6nI > article > div > div > div:nth-child(1) > div:nth-child(1) > a > div.eLAPa").click()
@@ -184,7 +179,7 @@ class InstagramBot:
                 except Exception as e:
                     print("Failed to wait for person's profile to load>>>")
                     print(e)
-                
+
                 self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.ltpMr.Slqrh > span.fr66n > button").click()
                 print("Media liked.")
                 sleep(math.floor(random.random() * 2 + 1))
@@ -194,35 +189,39 @@ class InstagramBot:
                 commentBox = self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > textarea")
                 commentBox.send_keys(commentText)
                 print("Media commented.")
-                sleep(math.floor(random.random() * 3 + 2))
+                sleep(math.floor(random.random() * 2 + 2))
+                number_of_comments_before = self.getNumberOfComments()
                 # Hit the post button
-                self.driver.find_element_by_css_selector("body > div._2dDPU.CkGkG > div.zZYga > div > article > div.eo2As > section.sH9wk._JgwE > div > form > button").click()
-                sleep(math.floor(random.random() * 3 + 2))
+                self.driver.find_element_by_xpath("//button[contains(text(),'Post')]").click()
+                sleep(random.random() * 2 + 4)
+                if not self.checkCommentDidPost(number_of_comments_before):
+                screenshot here
+                    print("!!!!!!!!!!!!!!!######### UNABLE TO POST COMMENT #########!!!!!!!!!!!!!!!")
+
                 print("Commented: " + commentText)
-                sleep(math.floor(random.random() * 3 + 1))
+                sleep(random.random() + .5)
                 return True
-                
-            except:
-                print('Commenting on post failed')
-                sleep(math.floor(random.random() + 0.3))
+
+            except Exception as e:
+                print('Commenting on post failed', e)
+                sleep(random.random() + 0.3)
                 return False
-        except:
-            print('Selecting latest post failed')
+        except Exception as e:
+            print('Selecting latest post failed', e)
             return False
-        
+
     def scrapeFollowers(self):
-        
+
         '''
             Returns a list of followers after scrolling through the modal.
         '''
-        
-        SCROLL_PAUSE = 0.6  # Pause to allow loading of content
+
+        SCROLL_PAUSE = 0.8  # Pause to allow loading of content
         self.driver.execute_script("followersbox = document.getElementsByClassName('isgrP')[0];")
         last_height = self.driver.execute_script("return followersbox.scrollHeight;")
-        
+
         # Finally, scrape the followers
         selector = "a.FPmhX"
-        
 
         # We need to scroll the followers modal to ensure that all followers are loaded
         while True:
@@ -243,48 +242,57 @@ class InstagramBot:
         print(followers_elems)
 
         followers = [e.text for e in followers_elems]  # List of followers
-    
+
         print("______________________________________")
         print("SCRAPED FOLLOWERS")
 
         return followers
-    
-    
+
+    # To be used to get the number of comments on a post
+    def getNumberOfComments(self):
+        return len(self.driver.find_elements_by_class_name("Mr508"))
+
+
+    # To be used to check if a comment was successfully posted
+    def checkCommentDidPost(self, num_comments_before):
+        return self.getNumberOfComments() > num_comments_before
+
+    # To be used to go to a instagram follower account page
     def goToFollower(self,follower):
+        sleep(random.random() + 0.5)
         self.driver.get(f"https://www.instagram.com/{follower}/")
-        sleep(random.random() + 0.4)
+        sleep(random.random() + 0.5)
 
 
     def doCommentRound(self):
-        
+
+        sleep(random.random() + 1)
         # Get follower names
         followers = self.scrapeFollowers()
         #print(followers)
-        
+
         comments_done = 0
         # For every folllower name, go to follower url and comment until comments_done reaches target amount.
         for follower in followers:
-            
+
             if follower in self.previousCommentsUsernames:
                 continue
-            
+
             self.goToFollower(follower)
-            
+
             comment = randomCommentText()
             if (self.is_main):
                 comment = randomCommentTextMain()
-            
+
             if (self.comment(comment)):
                 comments_done += 1
                 print("Comment: ", comments_done)
-                
-            if comments_done == 5:
-                break
-            
-            
-        
-        return True
+                print("Cummented on: ", follower)
 
+            if comments_done == 30:
+                break
+
+        return True
 
     def alreadyCommented(self, username):
         for commented_on_user in self.previousCommentsUsernames:
@@ -310,8 +318,8 @@ class InstagramBot:
             if (aria_label == "Unlike"):
                 return True
             return False
-        except:
-            print("Exception")
+        except Exception as e:
+            print("Exception", e)
             return False
 
 
@@ -319,8 +327,8 @@ class InstagramBot:
         sleep(math.floor(random.random() * 3 + 2))
         try:
             return (int(self.driver.find_element_by_css_selector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a > span").text.replace(',', '')) < 1500)
-        except:
-            print("Get number of followers failed")
+        except Exception as e:
+            print("Get number of followers failed", e)
             return False
 
     def quit(self):
@@ -329,15 +337,16 @@ class InstagramBot:
 if __name__ == '__main__':
     #4th argument indicates whether or not the account is the main account
 
-    user1 = "username"
-    user1_pass = "password"
+    user1 = ""
+    user1_pass = ""
 
     user3 =  "username"
     user3_pass = "password"
-    
+
     main1 = "genuineaesthetic"
     main1_pass = "MagicJohnson32!"
 
+    #ig_bot = InstagramBot(user1, user1_pass, 'zumiez', False)
     #ig_bot = InstagramBot(user2,user2_pass,'fuckit.jp',False)
     #ig_bot = InstagramBot(user3,user3_pass,'heartlessclothingco',False)
     #ig_bot = InstagramBot(user3,user3_pass,'vzn_clothing',False)
